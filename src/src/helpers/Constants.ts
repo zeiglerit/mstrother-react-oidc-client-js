8292cb60811ea4bf36e5abe40d503885596f09f5
Mark Strother <mark@pacific.ca>
2021-05-20 16:07:56 -0700

    Add roles

--- src/src/helpers/Constants.ts
+++ src/src/helpers/Constants.ts
@@ -2,7 +2,6 @@ export class Constants {
   public static stsAuthority = 'https://is5-sts-poc.azurewebsites.net';
   public static clientId = 'spa';
   public static clientRoot = 'https://salmon-island-08b39bd1e.azurestaticapps.net/';
-  public static clientScope = 'openid profile email api';
-
+  public static clientScope = 'openid profile email roles';
   public static apiRoot = 'https://demo.identityserver.io/api/';
 }
