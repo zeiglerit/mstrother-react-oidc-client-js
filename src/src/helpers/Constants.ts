cdd43b737be3fa2bf4bf66168cef0029662c9ecc
Mark Strother <mark@pacific.ca>
2021-05-20 15:20:35 -0700

    Update client ID

--- src/src/helpers/Constants.ts
+++ src/src/helpers/Constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
   public static stsAuthority = 'https://is5-sts-poc.azurewebsites.net';
-  public static clientId = 'interactive.public';
+  public static clientId = 'spa';
   public static clientRoot = 'https://salmon-island-08b39bd1e.azurestaticapps.net/';
   public static clientScope = 'openid profile email api';
 
