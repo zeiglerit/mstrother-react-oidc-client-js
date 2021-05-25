d405dca216ab2fcb357a4572746a0f4b88d414f3
Mark Strother <mark@pacific.ca>
2021-05-24 17:43:21 -0700

    Update constants

--- src/src/helpers/Constants.ts
+++ src/src/helpers/Constants.ts
@@ -2,6 +2,6 @@ export class Constants {
   public static stsAuthority = 'https://is5-sts-poc.azurewebsites.net';
   public static clientId = 'spa';
   public static clientRoot = 'https://salmon-island-08b39bd1e.azurestaticapps.net/';
-  public static clientScope = 'openid profile email roles';
-  public static apiRoot = 'https://demo.identityserver.io/api/';
+  public static clientScope = 'openid profile email roles api1';
+  public static apiRoot = 'https://netcoreapi-poc.azurewebsites.net/api/Identity';
 }
